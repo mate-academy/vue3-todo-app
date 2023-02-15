ba62a8800ce4f27b31effe49c9fc24ad251af2a4
Misha Hrynko <misha@mate.academy>
2023-02-15 18:43:36 +0200

    after TransitionGroup

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,22 @@
 import StatusFilter from './components/StatusFilter.vue';
 import TodoItem from './components/TodoItem.vue';
 
+function getFromLocalStorage(key, defaultValue) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (e) {
+    return defaultValue;
+  }
+}
+
 export default {
   components: {
     StatusFilter,
     TodoItem
   },
   data() {
-    let todos = [];
-
-    try {
-      todos = JSON.parse(
-        localStorage.getItem('todos') || '[]'
-      );
-    } catch (e) { }
-
     return {
-      todos,
+      todos: getFromLocalStorage('todos', []),
       title: '',
       status: 'all',
     };
@@ -26,6 +26,21 @@ export default {
     activeTodos() {
       return this.todos.filter(todo => !todo.completed);
     },
+    completedTodos() {
+      return this.todos.filter(todo => todo.completed);
+    },
+    visibleTodos() {
+      switch (this.status) {
+        case 'active':
+          return this.activeTodos;
+
+        case 'completed':
+          return this.completedTodos;
+
+        default:
+          return this.todos;
+      }
+    }
   },
   watch: {
     todos: {
@@ -49,6 +64,43 @@ export default {
 };
 </script>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <template>
   <div class="todoapp">
     <h1 class="todoapp__title">todos</h1>
@@ -70,15 +122,19 @@ export default {
         </form>
       </header>
 
-      <section class="todoapp__main">
+      <TransitionGroup
+        name="list"
+        tag="section"
+        class="todoapp__main"
+      >
         <TodoItem
-          v-for="todo, index of todos"
+          v-for="todo, index of visibleTodos"
           :key="todo.id"
           :todo="todo"
-          @update="todos[index] = $event"
-          @delete="todos.splice(index, 1)"
+          @update="Object.assign(todo, $event)"
+          @delete="todos.splice(todos.indexOf(todo), 1)"
         />
-      </section>
+      </TransitionGroup>
 
       <footer class="todoapp__footer">
         <span class="todoapp__active-count">
@@ -110,4 +166,16 @@ export default {
 </template>
 
 <style>
+.list-enter-active,
+.list-leave-active {
+  max-height: 60px;
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  max-height: 0;
+  transform: scaleY(0);
+}
 </style>
+
